02df96371c6a2c1074c73057df9c66636a7a3d05
dsoma5749-commits <your_email@example.com>
2026-06-19 11:16:02 +0530

    Add gitignore, remove target folder

--- target/debug/deps/akira_kernel-4913598c526eb335.d
+++
@@ -1,7 +0,0 @@
-/data/data/com.termux/files/home/akira_kernel/target/debug/deps/akira_kernel-4913598c526eb335.d: src/main.rs src/libs/kernel_core.rs src/scheduler.rs
-
-/data/data/com.termux/files/home/akira_kernel/target/debug/deps/akira_kernel-4913598c526eb335: src/main.rs src/libs/kernel_core.rs src/scheduler.rs
-
-src/main.rs:
-src/libs/kernel_core.rs:
-src/scheduler.rs:
